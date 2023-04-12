8476420d8068093bd507504ea7fe14c6cba990d3
Mihai Alexandru <77043862+MAJigsaw77@users.noreply.github.com>
2023-04-12 14:50:55 +0300

    Update Types.hx

--- hxwren/Types.hx
+++ hxwren/Types.hx
@@ -10,7 +10,6 @@ class Types {} // blank
 //
 // Wren has no global state, so all state stored by a running interpreter lives
 // here.
-
 @:buildXml('<include name="${haxelib:hxwren}/project/Build.xml" />')
 @:include("wren.hpp")
 @:keep
@@ -22,7 +21,6 @@ extern class WrenVM {}
 // This lets code outside of the VM hold a persistent reference to an object.
 // After a handle is acquired, and until it is released, this ensures the
 // garbage collector will not reclaim the object it references.
-
 @:buildXml('<include name="${haxelib:hxwren}/project/Build.xml" />')
 @:include("wren.hpp")
 @:keep
