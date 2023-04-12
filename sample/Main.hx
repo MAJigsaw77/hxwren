dd0e9b05fadd4960dc8e49a62e337962566c534a
Mihai Alexandru <77043862+MAJigsaw77@users.noreply.github.com>
2023-04-12 17:33:48 +0300

    From curiosity

--- sample/Main.hx
+++ sample/Main.hx
@@ -10,7 +10,7 @@ class Main
 {
 	private static function writeFn(vm:cpp.RawPointer<WrenVM>, text:cpp.ConstCharStar):Void
 	{
-		Sys.println(cast(text, String));
+		Sys.println(text.toString());
 	}
 
 	/*private static function errorFn(vm:cpp.RawPointer<WrenVM>, errorType:WrenErrorType, module:cpp.ConstCharStar, line:Int, msg:cpp.ConstCharStar):Void
