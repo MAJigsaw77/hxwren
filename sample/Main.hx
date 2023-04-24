1a2b36519d92ea71c5cc61ab21574b5c38d04074
Mihai Alexandru <77043862+MAJigsaw77@users.noreply.github.com>
2023-04-24 21:36:54 +0300

    Update Main.hx

--- sample/Main.hx
+++ sample/Main.hx
@@ -13,6 +13,19 @@ class Main
 		Sys.println(cast(text, String));
 	}
 
+	private static function errorFn(vm:cpp.RawPointer<WrenVM>, errorType:Int, module:cpp.ConstCharStar, line:Int, msg:cpp.ConstCharStar):Void
+	{
+		switch (cast(errorType, WrenErrorType))
+		{
+			case WREN_ERROR_COMPILE:
+				Sys.println('[' + cast(module, String) + ' line ' + line + '] [Error] ' + cast(msg, String));
+			case WREN_ERROR_STACK_TRACE:
+				Sys.println('[' + cast(module, String) + ' line ' + line + '] in ' + cast(msg, String));
+			case WREN_ERROR_RUNTIME:
+				Sys.println('[Runtime Error] ' + cast(msg, String));
+		}
+	}
+
 	public static function main():Void
 	{
 		Sys.println('Wren ${Wren.GetVersionNumber()}');
@@ -20,6 +33,7 @@ class Main
 		var config:WrenConfiguration = WrenConfiguration.create();
 		Wren.InitConfiguration(cpp.RawPointer.addressOf(config));
 		config.writeFn = cpp.Function.fromStaticFunction(writeFn);
+		config.errorFn = cpp.Function.fromStaticFunction(errorFn);
 
 		var vm:cpp.RawPointer<WrenVM> = Wren.NewVM(cpp.RawPointer.addressOf(config));
 
